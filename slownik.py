fac3c0f2acef26f7e96fb3da78dffe440f66d8c1
LukaszChojnowski246 <chojnowskilukasz9@gmail.com>
2026-03-03 13:01:54 +0100

    Add files via upload

---
+++ slownik.py
@@ -0,0 +1,73 @@
+import string
+import random
+
+
+print(string.ascii_uppercase)
+print(string.digits)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#utworz_samochod = {'Kolor': '','Marka': '','Rocznik': '', 'Silnik': '','Rodzaj paliwa': ''}
+#def samochod():
+#    utworz_samochod['Kolor'] = input('Podaj kolor samochodu: ')
+#    utworz_samochod['Marka'] = input('Podaj marke: ')
+#    utworz_samochod['Rocznik'] = input('Podaj rocznik wyjścia samochodu: ')
+#    utworz_samochod['Silnik'] = input('Podaj rodzaj silnika: ')
+#    utworz_samochod['Rodzaj paliwa'] = input('Podaj rodzaj paliwa: ')
+
+#    return utworz_samochod
+
+#def slownik(xd):
+#    for i in xd.keys():
+#        print(i, xd[i])
+
+#slownik(samochod())
+
+
+
+#wielolinijkowy_tekst = """Bardzo długi tekst """
+
+
+
+#def lol():
+#    """
+#    funkcja robiaca lol
+#    """
+#    for _ in range(1):
+#        print("l\o\lol")
+#lol()
+
+
+#imie = "kacper"
+#stopien = "Oficer"
+#wiek = 34
+#print(f"W 13 Posterunku, {imie} Koperek miał stopień {stopien}, a aktualnie ma lat {wiek}")
+
+
+
+
